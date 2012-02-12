5b662eafd4454acae2b9ed929fc9b83e5b716bfd
Oleksandr Kozachuk <ddeus.sf@mailnull.com>
2012-02-12 19:02:53 +0100

    Build script improvements.

--- vm/embedded/avr/arduino/build_binaries.sh
+++ vm/embedded/avr/arduino/build_binaries.sh
@@ -10,18 +10,17 @@ err () {
 [ X"$BOARD" = X"" ] && err "build_binaries.sh should be included in configuration."
 
 echo "Build image file"
-make -C $ROOT_DIRECTORY retro image >error.log 2>&1 || err "Failed to builed original retro"
-cat $MODULES \
-| $ROOT_DIRECTORY/retro --shrink --image $ROOT_DIRECTORY/retroImage >error.log 2>&1 \
-|| err "Failed to build retro image"
-make -C $ROOT_DIRECTORY images >error.log 2>&1 || err "Failed to convert retro image"
+cp $ROOT_DIRECTORY/retroImage retroImage || err "Failed to copy retroImage file"
+make -C $ROOT_DIRECTORY retro >error.log 2>&1 || err "Failed to builed original retro"
+cat $MODULES | $ROOT_DIRECTORY/retro --shrink --image retroImage >retro.log 2>&1 || err "Failed to build retro image"
+mv retro.log image.log
+$CC $CFLAGS -o convert $ROOT_DIRECTORY/tools/convert.c >error.log 2>&1 || err "Failed to compile convert tool"
+./convert >error.log 2>&1 || err "Failed to convert images"
 rm -f error.log
-ls -l $ROOT_DIRECTORY/retroImage16
+ls -l retroImage retroImage16
 
 echo "Create image header"
-hexdump -v \
-  -e "\"{\" $IMAGE_BLOCK_SIZE/2 \"%d,\" \"},\n\"" \
-  $ROOT_DIRECTORY/retroImage16 \
+hexdump -v -e "\"{\" $IMAGE_BLOCK_SIZE/2 \"%d,\" \"},\n\"" retroImage16 \
 | sed -e 's/,,* *},/}/' \
 | awk "BEGIN { curr = 0; }
 { print \"static const prog_int16_t image_\" curr \"[$IMAGE_BLOCK_SIZE] PROGMEM = \" \$0 \";\";
